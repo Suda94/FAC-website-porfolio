edbb484020f295f1f6e86fbea5e7c4c1794dd785
Suda94 <grg_sudarshan94@hotmail.com>
2022-11-26 19:28:24 +0000

    FAC_portfolio2022

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+//scripts for nav bar
+const navToggle = document.querySelector(".nav-toggle");
+const navlinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navlinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+
